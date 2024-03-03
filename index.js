b2b889dbd479ecf16b329e74560fd8dd71d61a11
Vanesa Edo <vanesa.en@gmail.com>
2024-03-03 17:51:52 +0100

    Carga de la promesa ok. Consulta y recuperación de datos del objeto ok. Representación en el DOM: pendiente.

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-const cardTemplate = function (/* You can pass the data here*/) {
-  return `<div class="card">
-              <img id="flag-image" src="ADD THE IMAGE LINK HERE" alt="flag" />
-              <h1 class="center">ADD COUNTRY NAME HERE</h1>
-            </div>`;
-};
+function countriesFlagName() {
 
-const countriesNode = document.getElementById("countries");
+  /* Need the provide API URL to get all countries */
+  //fetch('https://restcountries.com/v3.1/all')
 
-fetch(/* Need the provide API URL to get all countries */)
-  .then(function (response) {
+  return fetch('https://restcountries.com/v3.1/all')
+
+    //fetch('https://restcountries.com/v3.1/all?fields=name,flags')
     // fetch() returns a promise containing the response (a Response object).
     // This is just an HTTP response, not the actual JSON. 
     // To extract the JSON body content from the response, 
     // we use the json() method and pass it into the next .then()
-  })
-  .then(function (countries) {
+    .then(countries => countries.json())
     // Here is where you'll need to add into the DOM all the countries received from API 
-
     // 1 - We will need to iterate the countries variable with a loop
     // 2 - You can use the cardTemplate() function to create a div with a class card already styled
     // 💡 you can use countriesNode variable to add elements
-  });
+
+    .then(countriesObj => {
+      const countriesNode = document.getElementById("countries")
+      const cardTemplate = data => {
+        for (let i = 0; i < data.length; i++) {
+          `<div class="card">
+                    <img id="flag-image" src="${data[i].flags.png}" alt="${data[i].flags.alt}"/>
+                    <h1 class="center">${(data[i].name.common)}</h1>
+                  </div>`;
+          countriesNode.appendChild.cardTemplate
+        }
+        countriesNode.innerHTML += cardTemplate
+
+
+      }
+
+    });
+
+} 
